7349992c3b631e1ff771cf50f44f3a98d93505f5
russtone <russtone@yandex.ru>
2018-04-28 22:57:01 +0300

    Initial Commit

---
+++ CVSS/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  CVSS
+//
+//  Created by Anton Prokhorov on 28/04/2018.
+//  Copyright © 2018 Anton Prokhorov. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
